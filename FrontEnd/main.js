60cabd3077e6fdffb8702862f6c707c8d41aebed
Cédric Payet <cedricpayet.dev@gmail.com>
2024-09-29 13:00:03 +0200

    better edit-page : in progress

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -87,7 +87,6 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     } else if (currentPath.endsWith("login.html")) {
         const errorMessage = document.querySelector('.error-message');
-        console.log("1");
 
         document.querySelector('.login-form').addEventListener('submit', async function(e) {
             e.preventDefault();
@@ -162,4 +161,27 @@ window.addEventListener("keydown", function (e) {
     }
 });
 
+const token = window.localStorage.getItem("token");
+const editModeBar = document.getElementById("edit-mode-bar");
+const header = document.getElementsByTagName("header")[0];
+const loginLogoutButton = document.getElementById("login-logout-button");
+const modifyButton = document.getElementById("modify-button");
+
+const logout = function(e) {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    editModeBar.style.display = 'none';
+    header.style.marginTop = '50px';
+    loginLogoutButton.textContent = "login";
+    modifyButton.style.display = "none"
+    loginLogoutButton.removeEventListener("click", logout);
+}
+
+if (token) {
+    editModeBar.style.display = 'flex';
+    header.style.marginTop = '109px';
+    loginLogoutButton.textContent = "logout";
+    modifyButton.style.display = "inline"
+    loginLogoutButton.addEventListener("click", logout);
+}
 
