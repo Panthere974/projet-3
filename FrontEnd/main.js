46b9a59dbe6e8cf9ef2363f53628c120be977c65
Cédric Payet <cedricpayet.dev@gmail.com>
2024-09-29 08:25:30 +0200

    step 3.1 : in progress

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -89,8 +89,8 @@ document.addEventListener("DOMContentLoaded", () => {
         const errorMessage = document.querySelector('.error-message');
         console.log("1");
 
-        document.querySelector('.login-form').addEventListener('submit', async function(event) {
-            event.preventDefault();
+        document.querySelector('.login-form').addEventListener('submit', async function(e) {
+            e.preventDefault();
             const login = {
                 email: document.querySelector('input[name="email"]').value,
                 password: document.querySelector('input[name="password"]').value,
@@ -112,3 +112,54 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+let modal = null;
+
+const openModal = function (e) {
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'));
+    target.style.display = null;
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal", "true");
+    modal = target;
+    modal.addEventListener("click", closeModal);
+    modal.querySelector(".modal-close").addEventListener('click', closeModal);
+    modal.querySelector(".modal-stop").addEventListener('click', stopPropagation);
+    
+}
+
+const closeModal = function (e) {
+    if (modal == null) return;
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click", closeModal);
+    modal.querySelector(".modal-close").removeEventListener('click', closeModal);
+    modal.querySelector(".modal-stop").removeEventListener('click', stopPropagation);
+    modal = null;
+}
+
+const stopPropagation = function(e) {
+    e.stopPropagation();
+}
+
+const modalLink = document.querySelectorAll(".modal-link").forEach(link => {
+    link.addEventListener('click', openModal);
+});
+
+const focusInModal = function(e) {
+    e.preventDefault();
+    //à faire
+}
+
+window.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e);
+    }
+    if (e.key === "Tab" && modal !== null) {
+        focusInModal(e);
+    }
+});
+
+
