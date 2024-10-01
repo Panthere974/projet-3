35c02e40d59aff2351070ec3685891d7f852196f
Cédric Payet <cedricpayet.dev@gmail.com>
2024-10-01 18:04:24 +0200

    step 3.1 : in progress

--- FrontEnd/js/categories.js
+++ FrontEnd/js/categories.js
@@ -1,19 +1,35 @@
 export async function getAllWorks() {
     const gallery = document.querySelector('.gallery');
+    const modalGallery = document.querySelector('.modal-gallery');
     const response = await fetch('http://localhost:5678/api/works');
     const data = await response.json();
 
     data.forEach(work => {
         const figure = document.createElement('figure');
-        const img = document.createElement('img');
+        const imgGallery = document.createElement('img');
         const figcaption = document.createElement('figcaption');
 
-        img.src = work.imageUrl;
-        img.alt = work.title;
+        const imgContainer = document.createElement('div');
+        const trashButton = document.createElement('button');
+        const trash = document.createElement('i');
+        const imgModal = document.createElement('img');
+
+        imgGallery.src = work.imageUrl;
+        imgGallery.alt = work.title;
         figcaption.textContent = work.title;
-        figure.appendChild(img);
+        figure.appendChild(imgGallery);
         figure.appendChild(figcaption);
         gallery.appendChild(figure);
+
+        imgContainer.className = 'image-container';
+        trashButton.className = 'trash-button';
+        trash.className = 'fa-solid fa-trash-can fa-xs';
+        imgModal.src = work.imageUrl;
+        imgModal.alt = work.title;
+        imgContainer.appendChild(imgModal);
+        trashButton.appendChild(trash);
+        imgContainer.appendChild(trashButton);
+        modalGallery.appendChild(imgContainer);
     });
 }
 
