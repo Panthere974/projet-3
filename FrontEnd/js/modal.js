8f4f482d3183ab60de02f4a832afee1934a2f7a1
Cédric Payet <cedricpayet.dev@gmail.com>
2024-10-04 21:02:38 +0200

    step 3.3 : in progress

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -12,17 +12,41 @@ const focusInModal = function(e) {
 export const openAddImageModal = function (e) {
     e.preventDefault();
 
+    const modalNavBar = document.querySelector('.modal-nav-bar');
+    const modalReturn = document.querySelector('.modal-return');
     const modalTitle = document.getElementById('modal-title');
     const modalGallery = document.getElementById('modal-gallery');
     const modalinsertImage = document.getElementById('modal-insert-image');
     const modalFormElements = document.querySelectorAll('.modal-form-element');
 
+    modalNavBar.style.justifyContent = 'space-between';
+    modalReturn.style.display = null;
+    modalReturn.addEventListener('click', closeAddImageModal);
     modalTitle.textContent = 'Ajout photo';
     modalGallery.style.display = 'none';
     modalinsertImage.style.display = null;
     modalFormElements.forEach(element => element.style.display = null);
-    e.target.setAttribute('type', 'submit');
     e.target.value = 'Valider';
+}
+
+const closeAddImageModal = function (e) {
+    e.preventDefault();
+
+    const modalNavBar = document.querySelector('.modal-nav-bar');
+    const modalReturn = document.querySelector('.modal-return');
+    const modalTitle = document.getElementById('modal-title');
+    const modalGallery = document.getElementById('modal-gallery');
+    const modalinsertImage = document.getElementById('modal-insert-image');
+    const modalFormElements = document.querySelectorAll('.modal-form-element');
+
+    modalNavBar.style.justifyContent = 'end';
+    modalReturn.style.display = 'none';
+    modalReturn.removeEventListener('click', closeAddImageModal);
+    modalTitle.textContent = 'Galerie photo';
+    modalGallery.style.display = null;
+    modalinsertImage.style.display = 'none';
+    modalFormElements.forEach(element => element.style.display = 'none');
+    e.target.value = 'Ajouter une photo';
 }
 
 export const openModal = function (e) {
